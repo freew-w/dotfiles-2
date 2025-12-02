7db52b3bf7aaabf6d9ae42682b477e870153cf44
free <freeadlau@gmail.com>
2025-12-03 03:00:29 +0800

    Set wrap text, remove ignored form snacks picker

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,14 +1,11 @@
 return {
-  {
-    "folke/snacks.nvim",
-    opts = {
-      picker = {
-        hidden = true,
-        ignored = true,
-        sources = {
-          files = {
-            hidden = true,
-          },
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      hidden = true,
+      sources = {
+        files = {
+          hidden = true,
         },
       },
     },
